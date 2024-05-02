854dd4244ea0f0b116cfaf0a8c872c32a85314c0
Pinnoche <ptime552@gmail.com>
2024-05-02 09:05:07 -0400

    logout fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            '/logout'
+        ]);
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        ],);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
